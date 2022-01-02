8966777054fd170d6731fd97ec2815425f7aca94
Crux One <Crux-One@users.noreply.github.com>
2022-01-02 23:26:03 +0900

    feat: add support for running actix_web

--- tiny-runner/src/main.rs
+++ tiny-runner/src/main.rs
@@ -1,11 +1,42 @@
-use std::net::TcpListener;
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use json::JsonValue;
+use serde::{Deserialize, Serialize};
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
 
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+/// This handler uses json extractor
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    println!("model: {:?}", &item);
+    HttpResponse::Ok().json(item.0) // <- send response
+}
 
-        println!("Connection established!");
-    }
+/// This handler uses json extractor
+async fn json(body: web::Bytes) -> Result<HttpResponse, Error> {
+    let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
+
+    let injson: JsonValue = match result {
+        Ok(v) => v,
+        Err(e) => json::object! {"err" => e.to_string() },
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(injson.dump()))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
+            .service(web::resource("/json").route(web::post().to(json)))
+            .service(web::resource("/").route(web::post().to(index)))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
